405da207406c064c65634a1455aaeec0ceca1172
hym19940701 <412947075@qq.com>
2017-10-29 21:21:03 +0800

    餐饮供应链管理平台

---
+++ js/login.js
@@ -0,0 +1,50 @@
+﻿//---控制登录窗口显示隐藏---
+$('.top-btn-login').on('click', function () {
+    $('.login').show('fast');
+    $('.home').css('backgroud-color', 'rgba(0,0,0,.7)');
+    $('.login').css('opacity', '1')
+})
+$('.btn-login-close').on('click', function () {
+    $('.login').hide('fast');
+    $('.home').css('opacity', '1');
+})
+
+//简单的登录验证
+++function () {
+    var userCount = [];
+    var username = [];
+    var pwd = [];
+    var flag = false;
+    $('#btn-submit').click(function () {
+        $.get('./login.txt', function (data, status) {
+            var data = JSON.parse(data);
+            var user = Object.keys(data);
+            for (let i in user) {
+                var count = user[i];
+                userCount[i] = data[count][0];
+                userCount[i].昵称 = count
+            }
+            console.log(userCount)
+            //获取输入的用户名密码
+            var getUserName = $('#username').val();
+            var getpwd = $('#pwd').val();
+
+            for (let i in userCount) {
+                if (getUserName == userCount[i].qq账号) {
+                    flag = true
+                    var pwd = userCount[i].qq密码
+                    console.log(pwd)
+                    if (getpwd == pwd) {
+                        console.log('登录成功')
+                        //获取账号昵称
+                        var cilent = userCount[i].昵称
+                        console.log(cilent)
+                        window.open('managePage.aspx?name=' + cilent, '_self')
+                    }
+                }
+
+            }
+            flag === false && console.log('用户名不存在');
+        })
+    })
+}()
---
+++ js/managePage.js
@@ -0,0 +1,58 @@
+﻿
+
+//侧边栏显示隐藏
+//在使用animat时通过给侧边栏和主内容设置不同的动画速率，来规避同步动画时它们之间产生的间隙
+~~function () {
+    var flag = true;
+    $('.navbar-header .nav-btn').on('click', function () {
+        flag && $('.sidebar').animate({ 'width': '0' },450,'swing', function () { flag = false });
+        flag && $('.content-main').animate({ 'padding-left': '0' }, 300, function () { flag = false });
+        !flag && $('.content-main').animate({ 'padding-left': '196px' }, 450, 'swing', function () { flag = true });
+        !flag && $('.sidebar').animate({ 'width': '196px' },300, function () { flag = true });
+    })
+}()
+
+
+//侧边栏样式交互
+~~function () {
+    $('.a1>a').on('click', function () {
+        var imgsrc = $(this).find('.arrow-icon').attr('src');
+        imgsrc === 'static/img/vectorgraph/arrow-bottom.png'
+        ? $(this).find('.arrow-icon').attr('src', 'static/img/vectorgraph/arrow-left.png')
+        : $(this).find('.arrow-icon').attr('src', 'static/img/vectorgraph/arrow-bottom.png');
+        $(this).parent().addClass('active-a1').siblings().removeClass('active-a1');
+        console.log($(this).parent().find('ul'));
+        $(this).parent().find('.nav-scond-level .a2').toggle();
+        $(this).parent().siblings().find('.nav-scond-level .a2').css('display', 'none')
+    })
+}()
+
+var homelink = $('#homelink').attr('href')
+var href = homelink + window.location.search
+$('#homelink').attr('href', href)
+$('#homelink')[0].click()
+//动态添加侧边栏
+// var a1 = [];
+// var a2 = [];
+// $.get('./sidebar.txt', function (data, stauts) {
+//     var sidebarData = JSON.parse(data).sidebar
+//     for (var i in sidebarData) {
+//         //console.log(sidebarData[i])
+//         var key = Object.keys(sidebarData[i])[0];
+//         var value=sidebarData[i][key]
+//         a1.push(key)
+//         a2.push(value)
+//         console.log(key,sidebarData[i][key])
+//     }
+//     var ja1span = $('.a1>a>span')
+//     for (var i in ja1span) {
+//         ja1span.eq(i).html(a1[i])
+//         var ja2span = ja1span.eq(i).parent().parent()
+//         console.log('ja2', ja2span[0], i)
+//         if (ja2span.find('.a2')) {
+//             for (var j in ja2span.find('.a2')) {
+//                 ja2span.find('.a2').eq(j).find('span').html(a2[i][j])
+//             }
+//         }
+//     }
+// })
